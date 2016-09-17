36c169f049b7af24de2d48714c1482022b044b00
pddivine <pddivine@gmail.com>
2016-09-16 19:44:45 -0700

    Added test for postgres DB connection.

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const config  = require('config');
 const http    = require('http');
 const express = require('express');
 const app     = express();
+const pgHdlr  = require('data/postgresHandler.js');
 const router  = express.Router();
 
 //Middleware
