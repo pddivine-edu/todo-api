43c47139497c33b710e4f04b81091b01a57a2632
pddivine <pddivine@gmail.com>
2016-09-17 10:49:45 -0700

    Update database to "todo" in prod creds.

--- data/postgresHandler.js
+++ data/postgresHandler.js
@@ -9,7 +9,7 @@ pool.connect(function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+  client.query('SELECT $1::int AS number', ['777'], function(err, result) {
     //call `done()` to release the client back to the pool
     done();
 
