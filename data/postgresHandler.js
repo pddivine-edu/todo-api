6a5fc7c84aae0effd300fa321c5ea4403c059fb5
pddivine <pddivine@gmail.com>
2016-09-18 10:45:43 -0700

    Update postgres to log rows from "todo" table.

--- data/postgresHandler.js
+++ data/postgresHandler.js
@@ -9,14 +9,15 @@ pool.connect(function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query('SELECT $1::int AS number', ['777'], function(err, result) {
+    //'SELECT $1::int AS number'
+  client.query('SELECT * FROM TODO', function(err, result) {
     //call `done()` to release the client back to the pool
     done();
 
     if(err) {
       return console.error('error running query', err);
     }
-    console.log(result.rows[0].number);
+    console.log(result.rows);
     //output: 1
   });
 });
